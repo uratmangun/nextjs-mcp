a6100ce0af13e315b083c559358fff26967783fc
uratmangun <17805677+uratmangun@users.noreply.github.com>
2025-09-06 09:06:02 +0700

    ✨ feat(integration): add xmcp and farcaster miniapp sdk integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+'use client';
+
 import Image from "next/image";
+import { sdk } from '@farcaster/miniapp-sdk'
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const initializeSdk = async () => {
+      await sdk.actions.ready();
+    };
+    initializeSdk();
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
